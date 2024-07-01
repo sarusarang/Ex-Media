8fd2c5106915099d4daba61c86669e0144625ea8
sarusarang <sarangsaru445@gmail.com>
2024-07-01 10:19:18 +0530

    finished

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -70,7 +70,7 @@ function Services() {
         </div>
 
 
-        <div className='our-service w-100 container mt-5 p-2' data-aos="fade-up" data-aos-duration="1000">
+        <div className='our-service w-100 container mt-5 p-2 mb-5' data-aos="fade-up" data-aos-duration="1000">
 
           <h1 className='text-center'>OUR SERVICES</h1>
           <p className='text-center mb-5'>WE CREATE BEST PRODUCTS</p>
@@ -78,9 +78,9 @@ function Services() {
           <div className='row'  >
 
 
-            <div className='col-md-4' data-aos="fade-up" data-aos-duration="1000">
+            <div className='col-md-4 col-12 col1' data-aos="fade-up" data-aos-duration="1000">
 
-              <Card style={{ width: '25rem' }}>
+              <Card style={{ width: '26rem' }}>
 
                 <Card.Img variant="top" src="/CARD-1.jpg" />
 
@@ -100,11 +100,243 @@ function Services() {
 
                 </Card.Body>
 
+              </Card>
+
+            </div>
+
+
+
+            <div className='col-md-4 col-12 col1' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="/CARD-2.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>PROMOTIONAL & BRAND VIDEO CREATION</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000">
+
+                    Crafting compelling promotional videos that powerfully convey brand messaging,
+                    evoking emotions and driving engagement. A strategic visual tool for modern marketing, capturing attention and fostering connections.
+
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
 
               </Card>
 
+            </div>
+
+
+
+
+
+
+            <div className='col-md-4 col-12 col1' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="/CARD-3.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>BRANDING GRAPHICS</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000">
+
+                    Capturing products' essence with skillful visuals for effective marketing impact. Product photography expertly captures items, enhancing their appeal through lighting, composition, and editing.
+                    Crucial for marketing, e-commerce, and conveying product qualities to potential customers.
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
 
             </div>
+
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="https://www.exmedia.in/assets/images/services/ui.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>PRODUCT & SERVICE GRAPHICS</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000">
+
+                    Products and service graphics visually communicate offerings, illustrating features and benefits. Enhancing understanding, these visuals aid marketing, presentations, and customer
+                    engagement, driving successful communication and business growth
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
+
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="https://www.exmedia.in/assets/images/services/handp.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>BUSINESS STRATEGY DEVELOPMENT</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000" className='pb-5 pt-3'>
+
+                    We Can formulate comprehensive plans that define a company's goals, tactics, and direction, guiding
+                    decisions to achieve sustainable growth and competitive advantage.
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
+
+
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="https://www.exmedia.in/assets/images/services/future.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>MARKET RESEARCH & ANALYTICS</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000" className='pb-3'>
+
+                    exmedia will do Market research and analytics involving collecting, analyzing, and interpreting data to gain insights into consumer
+                    behaviors and market trends, aiding informed business decisions and strategy refinement.
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
+
+
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top"   src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2022/01/104228210.jpg?auto=format&q=60&fit=max&w=930" />
+
+
+                <Card.Body>
+
+                  <Card.Title>WEB & MOBILE APP DEVELOPMENT</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000" className='pb-5'>
+
+                    Unlock your online potential with a dynamic multifaceted & high-performing website. We blend innovation
+                    and aesthetics to craft websites that elevate brands, engage visitors, and deliver exceptional user experiences.
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top" src="https://www.exmedia.in/assets/images/services/digitalonline.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>DIGTAL MARKETING</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000" className='pb-3'>
+
+                    We can maximize any business and online presence with Digital marketing through strategic campaigns across
+                    platforms. It includes SEO, social media, content, and analytics to reach, engage, and convert target audiences effectively.
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
+
+
+
+            <div className='col-md-4 col-12 col1 mt-5' data-aos="fade-up" data-aos-duration="1000">
+
+              <Card style={{ width: '26rem' }}>
+
+                <Card.Img variant="top"  src="https://www.exmedia.in/assets/images/services/ai.jpg" />
+
+
+                <Card.Body>
+
+                  <Card.Title>SATELLITE MEDIA</Card.Title>
+
+                  <Card.Text data-aos="fade-up" data-aos-duration="1000" className='pb-5'>
+
+                    Works to Elevate business through Satellite Media. Harness upgrade technology
+                    to expand reach, enhance communication, and connect with audiences on a global scale for unprecedented growth
+
+
+                  </Card.Text>
+
+
+                </Card.Body>
+
+              </Card>
+
+            </div>
+
 
 
           </div>
